2877e40c9e212e1aeb8aa7594c1cee929d17e692
vincent vincent <vincentalexx03@gmail.com>
2023-12-16 20:25:49 +0700

    slick

--- src/components/Slick.jsx
+++ src/components/Slick.jsx
@@ -1,12 +1,62 @@
+import $ from 'jquery'
+import './Slick.css'
+import { Button, Card } from 'react-bootstrap';
+import { SlickList } from './SlickList';
 
 
 const Slick = () => {
+    $(document).ready(function(){
+        $('.slick').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            responsive: [
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              },
+              {
+                breakpoint: 640,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+            ]
+        });
+      });
+
     return (
-        <div>
-            <div class="your-class text-light">
-                <div>your content</div>
-                <div>your content</div>
-                <div>your content</div>
+        <div className='d-flex flex-column justify-content-center mw-50 px-[80px]' style={{paddingLeft: '80px', paddingRight: '80px'}}>
+            <div className="slick text-light d-flex justify-content-center">
+              {SlickList.map((slick) => {
+                return (
+                  <a href="#">
+                    <Card style={{ maxWidth: '390px', marginRight: '30px', backgroundColor: 'black'}} className='rounded pb-2 d-flex justify-content-center'>
+                      <Card.Img variant="top" src={slick.img} />
+                      <Card.Body style={{height: '390px'}} className='border rounded bg-light'>
+                        <Card.Title className='text-black bg-light'>{slick.title}</Card.Title>
+                        <Card.Text className='text-black bg-light'>{slick.text}</Card.Text>
+                        <a href="#" className='text-decoration-none bg-light text-warning font-weight-bold h5'>Show Notes  </a>
+                      </Card.Body>
+                    </Card>
+                  </a>
+                );
+                })}
             </div>
 				
         </div>
---
+++ src/components/SlickList.jsx
@@ -0,0 +1,62 @@
+import jessica from '../assets/jessica.jpeg'
+import mike from '../assets/mike.jpeg'
+import robbie from '../assets/robbie.jpeg'
+import brett from '../assets/brett.jpeg'
+import luke from '../assets/luke.jpeg'
+
+export const SlickList = [
+    {
+        id : '1',
+        img : jessica,
+        title : '20 Minute Playbook - Jessica Schaefer of Beveland Acorns',
+        text : 'Jessica Schaefer is CEO and Founder of Bevel, a strategic communications firm with a portfolio in fintech and venture capital, as well as CCO of Acorns. In this episode, Jessica and Daniel discuss habits, routines, and inspirations.',
+    },
+    {
+        id : '2',
+        title : '20 Minute Playbook – Mike Clare of Mood Health',
+        img : mike,
+        text : 'Mike Clare is Founder and CEO of Mood Health, an online platform providing psychiatry, talk therapy, and medication management for depression and anxiety. In this episode, Mike and Daniel discuss Mike’s work at Juxtapose and his habits and routines.',
+    },
+    {
+        id : '3',
+        img : mike,
+        title : 'Infinite Games – Rethinking Mental Health Care with Mike Clare of Mood Health',
+        text : 'Mike Clare is Founder and CEO of Mood Health, an online platform providing psychiatry, talk therapy, and medication management for depression and anxiety. In this episode, Mike and Daniel discuss the pain points of traditional mental health care and how Mood aims to alleviate them.',
+    },
+    {
+        id : '4',
+        img : robbie,
+        title : '20 Minute Playbook – Robbie Bent of Othership',
+        text : 'Robbie Bent is CEO and Co-Founder of Othership, a platform for breathwork, connection, and mental wellness. In this episode, Robbie and Daniel discuss habits, routines, and inspirations.',
+    },
+    {
+        id : '5',
+        img : jessica,
+        title : 'Infinite Games – Crafting a Business Narrative with Jessica Schaefer of Bevel and Acorns',
+        text : 'Jessica Schaefer is CEO and Founder of Bevel, a strategic communications firm with a portfolio in fintech and venture capital, as well as CCO of Acorns. In this episode, Jessica and Daniel discuss building executive profiles, crafting a company narrative, and why PR is a smart investment.',
+    },
+    {
+        id : '6',
+        img : brett,
+        title : '20 Minute Playbook – Brett Goldstein of Launch House',
+        text : 'Brett Goldstein is Co-Founder of Launch House, a private community focused on connecting and supporting top entrepreneurs. In this episode, Brett and Daniel discuss habits, routines, and inspirations.',
+    },
+    {
+        id : '7',
+        img : robbie,
+        title : 'Infinite Games – Curating Peak Experiences of Belonging with Robbie Bent of Othership',
+        text : 'Robbie Bent is CEO and Co-Founder of Othership, a platform for breathwork, connection, and mental wellness. In this episode, Robbie and Daniel discuss creating and scaling a physical business, advice for high achievers, and the pitfalls of comparison.',
+    },
+    {
+        id : '8',
+        img : luke,
+        title : '20 Minute Playbook – Luke Gromen of The Forest for the Trees',
+        text : 'Luke Gromen is CEO and Founder of FFTT, a macroeconomic research firm. In this episode, Luke and Daniel discuss habits, routines, and inspirations.',
+    },
+    {
+        id : '9',
+        img : brett,
+        title : 'Infinite Games – Building the Next Y Combinator with Brett Goldstein of Launch House',
+        text : 'Brett Goldstein is Co-Founder of Launch House, a private community focused on connecting and supporting top entrepreneurs. In this episode, Brett and Daniel discuss innovation, the future of education, and vibes as a moat.',
+    },
+];
